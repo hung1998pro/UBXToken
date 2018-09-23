2384407a5fbdb28a84d3ad03829b1be5239dec0c
Stranger <Stranger>
2018-09-23 10:18:54 +0700

    3. Delegated transfer

--- contracts/UBXToken.sol
+++ contracts/UBXToken.sol
@@ -12,8 +12,15 @@ contract UBXToken{
         address indexed _to,
         uint256 _value
     );
+    //transfer
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
     mapping(address=> uint256) public balanceOf;
-
+    mapping(address=>mapping(address=>uint256)) public allowance;
+    //allowance 
     //function UBXToken(uint256 _initialSupply) public {
       constructor(uint256 _initialSupply) public{
         balanceOf[msg.sender]=_initialSupply;
@@ -28,5 +35,27 @@ contract UBXToken{
         balanceOf[_to]+=_value;
         emit Transfer(msg.sender,_to,_value);
         return true;
+    }
+    //approve
+    function approve(address _spender,uint256 _value)public returns (bool success){
+        //allowance
+        allowance[msg.sender][_spender]=_value;
+        emit Approval(msg.sender,_spender,_value);
+        return true;
+    }
+    function transferFrom(address _from,address _to,uint256 _value) public returns (bool success){
+    require(_value<=balanceOf[_from]);
+    //Require _from has enough tokens
+    require(_value<=allowance[_from][msg.sender]);// mesg.sender represent _from to make a transaction
+    //Require allowance is big enough
+    balanceOf[_from]-=_value;
+    balanceOf[_to]+=_value;
+    //change& update the balance
+    allowance[_from][msg.sender]-=_value;
+    //update the allowance
+    emit Transfer(_from,_to,_value);
+    //transfer event
+    return true;    
+    //return a booleean
     }
 }
--- test/UBXToken.js
+++ test/UBXToken.js
@@ -3,8 +3,8 @@ var UBXToken = artifacts.require("./UBXToken.sol"); //chuyen sang nodejs
 contract('UBXToken',function(accounts){
   var tokenInstance;
     it('initializes the contract with the correct values',function(){
-      return UBXToken.deployed().then(function(instance){
-        tokenInstance=instance;
+      return UBXToken.deployed().then(function(insect){
+        tokenInstance=insect;
         return tokenInstance.name(); 
       }).then(function(name){
         assert.equal(name,'UBX Token','has the exact name');
@@ -17,8 +17,8 @@ contract('UBXToken',function(accounts){
       });
     });
     it('allocates initial supply upon deployment',function(){
-        return UBXToken.deployed().then(function(instance){ //promise dai dien cho ket qua asynctask
-            tokenInstance = instance;
+        return UBXToken.deployed().then(function(insect){ //promise dai dien cho ket qua asynctask
+            tokenInstance = insect;
             return tokenInstance.totalSupply();
         }).then(function(totalSupply){
             assert.equal(totalSupply.toNumber(),66669999,'set the total supply to 66669999');
@@ -28,8 +28,8 @@ contract('UBXToken',function(accounts){
           });
     });
     it('transfers token ownership',function(){
-      return UBXToken.deployed().then(function(instance){
-        tokenInstance=instance;
+      return UBXToken.deployed().then(function(insect){
+        tokenInstance=insect;
         //test require statement first by transferring something larger than the sender balance
         //call would return true or false value
         return tokenInstance.transfer.call(accounts[1],9999999999999999);
@@ -54,4 +54,67 @@ contract('UBXToken',function(accounts){
         assert.equal(balance.toNumber(),66369999,'deducts the amount from sending account');
       });
     });
-})
+    it('approves tokens for delegated transfer',function(){
+      return UBXToken.deployed().then(function(insect){
+        tokenInstace=insect;
+        return tokenInstance.approve.call(accounts[1],100);
+      }).then(function(success){
+        assert.equal(success,true,'it returns true');
+        return tokenInstance.approve(accounts[1],100,{from: accounts[0]});
+      }).then(function(receipt){
+        assert.equal(receipt.logs.length,1,'triggers one event');
+        assert.equal(receipt.logs[0].event,'Approval','should be the "Approval" event');
+        assert.equal(receipt.logs[0].args._owner,accounts[0],'logs the account the token are authorized by');
+        assert.equal(receipt.logs[0].args._spender,accounts[1],'logs the account the tokens are authorized to');
+        assert.equal(receipt.logs[0].args._value,100,'logs the transfer amount');
+        return tokenInstace.allowance(accounts[0],accounts[1]);
+      }).then(function(allowance){
+        assert.equal(allowance.toNumber(),100,'stores the allowance for delegated transfer');
+      })
+    });
+    it('handles delegated token transfers',function(){
+      return UBXToken.deployed().then(function(insect){
+        tokenInstance=insect;
+        fromAccount = accounts[2];
+        toAccount = accounts[3];
+        spendingAccount = accounts[4];
+        //Transfer some tokens to fromAccount
+        return tokenInstance.transfer(fromAccount,100,{from: accounts[0]});
+      }).then(function(receipt){
+        //Approve spendingAccounts to spend 10 tokens from fromAccount
+        return tokenInstance.approve(spendingAccount,10,{from: fromAccount});
+      }).then(function(recript){
+        //Try transfering something larger than the sender's Balance
+         return tokenInstance.transferFrom(fromAccount,toAccount,6969,{from: spendingAccount});
+      }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert')>=0,'cannot transfer value larger than balance');
+        //Try transferring something larger than approved amount
+        return tokenInstance.transferFrom(fromAccount,toAccount,30,{from: spendingAccount});//<=balance but greater than authoried balance
+      }).then(assert.fail).catch(function(error){
+        assert(error.message.indexOf('revert')>=0,'cannot transfer value larger than approved amount');
+        //don't actually want to create a transaction use call
+        return tokenInstance.transferFrom.call(fromAccount,toAccount,5,{from: spendingAccount});
+      }).then(function(success){
+        assert.equal(success,true);
+        //want to create transaction and change the state of accounts, check the result before change state
+        return tokenInstace.transferFrom(fromAccount,toAccount,5,{from: spendingAccount});
+      }).then(function(receipt){
+        assert.equal(receipt.logs.length,1,'triggers one event');
+        assert.equal(receipt.logs[0].event,'Transfer','should be the "Transfer" event');
+        assert.equal(receipt.logs[0].args._from,fromAccount,'logs the account the token are transferred from');
+        assert.equal(receipt.logs[0].args._to,toAccount,'logs the account the tokens are transferred to');
+        assert.equal(receipt.logs[0].args._value,5,'logs the transfer amount');
+        return tokenInstance.balanceOf(fromAccount);
+      }).then(function(balance){
+        assert.equal(balance.toNumber(),95,'deducts the amount from the sending Account');
+        return tokenInstace.balanceOf(toAccount);
+      }).then(function(balance){
+        assert.equal(balance.toNumber(),5,'adds the amount from the receiving account')
+        return tokenInstance.allowance(fromAccount,spendingAccount);
+      }).then(function(allowance){
+        assert.equal(allowance.toNumber(),5,'deducts the amount from the allowance');
+      });
+    });
+}); 
+/* assert(error.message.indexOf('revaert')>=0,'error message must contain revert'); */
+/* .then(assert.fail).catch(function(error){ */
